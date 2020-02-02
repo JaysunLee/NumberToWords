b5ddc3f0e367c1eb88dbe7ec3d28f335764b5238
Jaysun Lee <lee.jaysun@gmail.com>
2020-02-02 18:03:57 +1100

    Handle range 10-19

--- Program.cs
+++ Program.cs
@@ -6,14 +6,54 @@ namespace NumberToWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(NumberToWords(0));
-            Console.WriteLine(NumberToWords(1));
-            Console.WriteLine(NumberToWords(8));
+            Console.WriteLine("0 = " + NumberToWords(0));
+            Console.WriteLine("1 = " + NumberToWords(1));
+            Console.WriteLine("8 = " + NumberToWords(8));
+            Console.WriteLine("10 = " + NumberToWords(10));
+            Console.WriteLine("19 = " + NumberToWords(19));
         }
 
         static string NumberToWords(int number)
         {
-            return Ones(number);
+            if (number < 10)
+            {
+                return Ones(number);
+            }
+            else if (number < 20)
+            {
+                return Teens(number);
+            }
+
+            return "";
+        }
+
+        static string Teens(int num)
+        {
+            switch (num)
+            {
+                case 10:
+                    return "ten";
+                case 11:
+                    return "eleven";
+                case 12:
+                    return "twelve";
+                case 13:
+                    return "thirteen";
+                case 14:
+                    return "fourteen";
+                case 15:
+                    return "fifteen";
+                case 16:
+                    return "sixteen";
+                case 17:
+                    return "seventeen";
+                case 18:
+                    return "eighteen";
+                case 19:
+                    return "nineteen";
+                default:
+                    throw new ArgumentException();
+            }
         }
 
         static string Ones(int num)
@@ -41,7 +81,7 @@ namespace NumberToWords
                 case 9:
                     return "nine";
                 default:
-                    return string.Empty;
+                    throw new ArgumentException();
             }
         }
     }
