c8ac424a775888a3d1d7fd3d547c9faa9db6c0a9
Jaysun Lee <lee.jaysun@gmail.com>
2020-02-02 17:55:38 +1100

    Handle range 0-9

--- Program.cs
+++ Program.cs
@@ -6,7 +6,43 @@ namespace NumberToWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(NumberToWords(0));
+            Console.WriteLine(NumberToWords(1));
+            Console.WriteLine(NumberToWords(8));
+        }
+
+        static string NumberToWords(int number)
+        {
+            return Ones(number);
+        }
+
+        static string Ones(int num)
+        {
+            switch (num)
+            {
+                case 0:
+                    return "zero";
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                default:
+                    return string.Empty;
+            }
         }
     }
 }
